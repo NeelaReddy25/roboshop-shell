553000cceb9e051ddf676f26e1d85b9c7a10c589
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-22 12:16:28 +0530

    good effects

--- db-tier/mongodb.sh
+++ db-tier/mongodb.sh
@@ -28,20 +28,20 @@ else
 fi
 
 # Setup the MongoDB repo file
-cp /home/ec2-user/roboshop-shell/db-tier/mongo.repo /etc/yum.repos.d/mongo.repo &>>LOGFILE
+cp /home/ec2-user/roboshop-shell/db-tier/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 VALIDATE $? "Setup mongoDB repo"
 
-dnf install mongodb-org -y &>>LOGFILE
+dnf install mongodb-org -y &>>$LOGFILE
 VALIDATE $? "Installing mongoDB org"
 
-systemctl enable mongod &>>LOGFILE
+systemctl enable mongod &>>$LOGFILE
 VALIDATE $? "Enabling mongod"
 
-systemctl start mongod &>>LOGFILE
+systemctl start mongod &>>$LOGFILE
 VALIDATE $? "Starting mongod"
 
-cp /home/ec2-user/roboshop-shell/db-tier/mongod.conf /etc/mongod.conf &>>LOGFILE
+cp /home/ec2-user/roboshop-shell/db-tier/mongod.conf /etc/mongod.conf&>>$LOGFILE
 VALIDATE $? "Update listen address"
 
-systemctl restart mongod &>>LOGFILE
+systemctl restart mongod &>>$LOGFILE
 VALIDATE $? "Restart mongod"
