322b26d28072f8df18ae214d34c0753797819b97
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-24 00:20:47 +0530

    good effects

--- route53.sh
+++ route53.sh
@@ -2,7 +2,7 @@
 ENV="mongodb"
 IP="172.31.44.9"
 
-aws route53 change-resource-record-sets \
+/usr/local/bin/aws route53 change-resource-record-sets \
     --hosted-zone-id Z001712433NLPH2AI8HH5 \
     --change-batch '{
         "Comment": "Testing creating an A record set",
